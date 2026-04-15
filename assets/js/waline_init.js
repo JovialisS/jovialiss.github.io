8230b9b872ee8b937732fbf0f4c2df336a3e2909
Jovial <142502164+JovialisS@users.noreply.github.com>
2026-04-15 23:36:07 +0800

    walinejs

--- assets/js/waline_init.js
+++ assets/js/waline_init.js
@@ -1,29 +1,31 @@
 //import { init } from 'https://unpkg.com/@waline/client@v2/dist/waline.mjs';
 // import { init } from 'https://npm.elemecdn.com/@waline/client@v2/dist/waline.mjs';
-import { init } from 'https://cdn.jsdelivr.net/npm/@waline/client@v3/dist/waline.js';
 
 
-init({
-    el: '#waline',
-    serverURL: 'https://jovially.netlify.app/.netlify/functions/comment',
-    comment: false,
-    reaction: true,
-    search: false,
-    pageview: true, 
-    lang: 'zh-CN',
-    updateVisitor: window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1',
-    emoji: [
-      '//unpkg.com/@waline/emojis@1.4.0/tieba',
-      '//unpkg.com/@waline/emojis@1.4.0/weibo',
-      '//unpkg.com/@waline/emojis@1.4.0/qq',
-      '//unpkg.com/@waline/emojis@1.4.0/bilibili',
-    //   '//unpkg.com/@waline/emojis@1.4.0/tw-emoji',
-    ],
-    texRenderer: (blockMode, tex) =>
-    window.MathJax.startup.adaptor.outerHTML(
-        window.MathJax.tex2svg(tex, {
-            display: blockMode,
-        }),
-    ),
-});
+// import { init } from 'https://cdn.jsdelivr.net/npm/@waline/client@v3/dist/waline.js';
+
+
+// init({
+//     el: '#waline',
+//     serverURL: 'https://jovially.netlify.app/.netlify/functions/comment',
+//     comment: false,
+//     reaction: true,
+//     search: false,
+//     pageview: true, 
+//     lang: 'zh-CN',
+//     updateVisitor: window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1',
+//     emoji: [
+//       '//unpkg.com/@waline/emojis@1.4.0/tieba',
+//       '//unpkg.com/@waline/emojis@1.4.0/weibo',
+//       '//unpkg.com/@waline/emojis@1.4.0/qq',
+//       '//unpkg.com/@waline/emojis@1.4.0/bilibili',
+//     //   '//unpkg.com/@waline/emojis@1.4.0/tw-emoji',
+//     ],
+//     texRenderer: (blockMode, tex) =>
+//     window.MathJax.startup.adaptor.outerHTML(
+//         window.MathJax.tex2svg(tex, {
+//             display: blockMode,
+//         }),
+//     ),
+// });
 
